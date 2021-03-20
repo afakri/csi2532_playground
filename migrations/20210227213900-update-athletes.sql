27f591826b88849b7264e70086a5dc51e264647d
ayman fakri <61848289+afakri@users.noreply.github.com>
2021-03-19 21:04:54 -0400

    cleanup

--- migrations/20210227213900-update-athletes.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE athletes
-RENAME COLUMN identified_gender TO gender;
-
-INSERT INTO schema_migrations (migration, migrated_at) VALUES
-('20210227213900-update-athletes.sql', '2021-02-27 21:39:00'); 
